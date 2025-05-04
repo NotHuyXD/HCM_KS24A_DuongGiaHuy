f30500036e677311b0bb760d81de86b030774e64
NotHuyXD <n24dtcn038@student.ptithcm.edu.vn>
2025-05-04 20:30:38 +0700

    Add files via upload

---
+++ HCM_KS24A_DuongGiaHuy/js/dashboard.js
@@ -0,0 +1,76 @@
+let users = JSON.parse(localStorage.getItem('users')) || [];
+let projects = JSON.parse(localStorage.getItem('projects')) || [];
+let currentUser;
+const taskTable = document.getElementById("task-table");
+
+window.onload = function() {
+  if(!users.some(user=>user.onLogin==true)){
+    window.location.href="../pages/login.html";
+    return;
+  }
+}
+
+users.forEach(user => {
+  if (user.onLogin) {
+    currentUser = user;
+    console.log(currentUser);
+  }
+});
+
+projects.forEach((project, index) => {
+  const relatedTasks = project.tasks.filter(task => task.assigner === currentUser.name);
+  if (relatedTasks.length === 0) return;
+
+  const section = document.createElement("tbody");
+  section.className = "task-section";
+  section.id = `project-${index}`;
+
+  // Dòng tiêu đề của project
+  section.innerHTML += `
+    <tr class="project-title" onclick="toggleList(${index})">
+      <td colspan="6"><strong>${project.name}</strong></td>
+    </tr>
+  `;
+
+  // Thêm từng task của project
+  relatedTasks.forEach((task) => {
+        let classPriority;
+        let progress;
+
+        if (task.priority === "Cao") {
+        classPriority = "high";
+        } else if (task.priority === "Trung Bình") {
+        classPriority = "medium";
+        } else {
+        classPriority = "low";
+        }
+
+        if (task.progress === "onTime") {
+          progress = "Đúng tiến độ";
+          } else if (task.progress === "atRisk") {
+          progress = "Có rủi ro";
+          } else {
+          progress = "Trễ hạn";
+          }
+    section.innerHTML += `
+      <tr class="task-${project.id-1}">
+        <td>${task.name}</td>
+        <td>${task.status}</td>
+        <td class="${classPriority}">${task.priority}</td>
+        <td>${task.beginDate}</td>
+        <td>${task.endDate}</td>
+        <td style="width:10%" class="${task.progress}">${progress}</td>
+      </tr>
+    `;
+  });
+
+  taskTable.appendChild(section);
+});
+
+function toggleList(index) {
+    const rows = document.querySelectorAll(`.task-${index}`);
+    rows.forEach(row => {
+      row.style.display = row.style.display === "none" ? "table-row" : "none";
+    });
+  }
+  
